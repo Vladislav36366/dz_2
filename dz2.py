92bc053b97e0b538fd0a00379457af8002177741
Vladislav36366 <mihailo04@mail.ru>
2024-11-08 12:44:38 +0500

    dz2.py

--- dz2.py
+++ dz2.py
@@ -2,12 +2,7 @@ class House:
     def __init__(self,name,number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-    def go_to(self, new_floor):
-        if new_floor <= self.number_of_floors:
-            for i in range(new_floor):
-                print(i+1)
-        else:
-            print('Такого этажа нет')
+        
     def __len__(self):
         return  self.number_of_floors
 
